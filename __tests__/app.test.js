5518f4c8cf1d44ccfc1346f8b41f6da59c1c2353
teyahbd <teyahbrennendavies@gmail.com>
2022-10-10 16:20:28 +0100

    add an extra test for get review by id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -57,6 +57,18 @@ describe("app", () => {
                     created_at: expect.any(String),
                   })
                 );
+                expect(review).toEqual({
+                  review_id: 2,
+                  title: "Jenga",
+                  designer: "Leslie Scott",
+                  owner: "philippaclaire9",
+                  review_img_url:
+                    "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+                  review_body: "Fiddly fun for all the family",
+                  category: "dexterity",
+                  created_at: "2021-01-18T10:01:41.251Z",
+                  votes: 5,
+                });
               });
           });
           test("400: responds with error when passed an invalid id", () => {
