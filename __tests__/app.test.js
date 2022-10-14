7240a70541de627f975bde56fe8bb0adde303f5b
teyahbd <teyahbrennendavies@gmail.com>
2022-10-14 15:35:35 +0100

    add two passing error handling tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -483,9 +483,27 @@ describe("app", () => {
     describe("/comments", () => {
       describe("/:comment_id", () => {
         describe("DELETE: /api/comments/:comment_id", () => {
-          test.only("204: responds with no content", () => {
+          test("204: responds with no content", () => {
             // can you test more to see if actually deleted??
             return request(app).delete("/api/comments/1").expect(204);
+          });
+        });
+        describe("Error Handling", () => {
+          test("400: responds with error when passed an invalid id", () => {
+            return request(app)
+              .delete("/api/comments/notAnId")
+              .expect(400)
+              .then(({ body: { message } }) => {
+                expect(message).toBe("Bad request");
+              });
+          });
+          test("404: responds with error when passed id that does not exist", () => {
+            return request(app)
+              .delete("/api/comments/100")
+              .expect(404)
+              .then(({ body: { message } }) => {
+                expect(message).toBe("Not found");
+              });
           });
         });
       });
--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,8 +1,17 @@
-const { removeCommentById } = require("../models/comments.models");
+const {
+  removeCommentById,
+  fetchCommentById,
+} = require("../models/comments.models");
 
 exports.deleteCommentById = (req, res, next) => {
-  console.log("controller");
-  removeCommentById(req.params.comment_id).then(() => {
-    res.status(204).send();
-  });
+  const promises = [
+    fetchCommentById(req.params.comment_id),
+    removeCommentById(req.params.comment_id),
+  ];
+
+  return Promise.all(promises)
+    .then((promises) => {
+      res.status(204).send();
+    })
+    .catch(next);
 };
